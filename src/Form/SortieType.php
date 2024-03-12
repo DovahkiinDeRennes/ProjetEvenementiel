6a22ba592e0b1f5d6c8ba72dc63fa48554fb6533
alexGhido <alex.iacona@hotmail.fr>
2024-03-12 16:11:25 +0100

    création template create et sortieType, modif entité sortie, et la version

---
+++ src/Form/SortieType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie :'
+            ])
+            ->add('dateHeureDebut', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('nbInscriptionMax', IntegerType::class, [
+                'label' => 'Nombre de places :',
+            ])
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée (en minutes) :',
+                'attr' => ['min' => 1, 'max' => 1440],
+            ])
+            ->add('description', TextAreaType::class, [
+                'label' => 'Description et infos :',
+            ])
+            ->add('lieuId', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('btnRegister', SubmitType::class, [
+                'label' => 'Enregistrer',
+            ])
+            ->add('btnPublish', SubmitType::class, [
+                'label' => 'Publier la sortie',
+            ])
+            ->add('btnCancel', SubmitType::class, [
+                'label' => 'Annuler',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
