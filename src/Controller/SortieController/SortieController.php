f2913360812348bdac96d12b29f19f4a9414e582
alexGhido <alex.iacona@hotmail.fr>
2024-03-12 16:04:47 +0100

    sortie controller

---
+++ src/Controller/SortieController/SortieController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\SortieController;
+
+
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/sortie')]
+class SortieController extends AbstractController
+{
+    #[Route('/create', name: 'sortie_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            $em->persist($sortie);
+            $em->flush();
+        }
+
+        return $this->render('sortie/create.html.twig', [
+            'sortieForm' => $sortieForm
+        ]);
+    }
+
+
+}
