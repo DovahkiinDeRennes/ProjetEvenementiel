a77511a5340196878031793eaa4c00143114aa6c
DovahkiinDeRennes <mickal.macit2023@campus-eni.fr>
2024-03-12 09:48:15 +0100

    Modification du .env. ajout du base.html.twig + home.html.twig et d'un controller pour test la vue

---
+++ src/Controller/IndexController/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\IndexController;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(name: 'home_')]
+class HomeController extends  AbstractController
+{
+    #[\Symfony\Component\Routing\Attribute\Route(path: '',name: 'home',methods:['GET'])]
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig');
+    }
+}
